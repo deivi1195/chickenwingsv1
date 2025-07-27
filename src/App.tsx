5c583a812c7e9635854cc4843564c57c21ae28d6
Deivi Gonzalez <deivig1195@gmail.com>
2025-07-27 13:15:30 -0400

    feat: Mejora en la interfaz del carrito y menú móvil

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import { CartProvider } from './context/CartContext';
 import { ProductProvider } from './context/ProductContext';
@@ -21,6 +22,7 @@ const App = () => (
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/shop" element={<Shop />} />
+              <Route path="/cart" element={<Cart />} />
               <Route path="/direccion" element={<Direccion />} />
               <Route path="*" element={<NotFound />} />              
             </Routes>
